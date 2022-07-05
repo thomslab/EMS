9003cc03e8149ad58b2b83577b33baf0cef0f54c
mitohida <hidayatthomi@gmail.com>
2022-07-05 23:37:43 +0700

    initial commit

--- ems.py
+++ ems.py
@@ -1,35 +1,46 @@
 import smtplib
+import time
+import schedule
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from datetime import datetime
 from email import encoders
 
-fromaddr = "thomihidayat1@gmail.com"
-toaddr = ["hidayatthomi@gmail.com","0001indahratna@gmail.com"]
-tgl = datetime.today().strftime("%d-%m-%y")
-# membuat MIMEMultipart
-print("membuat pesan....")
-msg = MIMEMultipart()
-msg['From'] = fromaddr
-msg['To'] = ", ".join(toaddr)
-msg['Subject'] = "EMS Daily Report "+ tgl
-body = "Please find the attached file here.."
-msg.attach(MIMEText(body, 'plain'))
-print("pesan jadi ...! melampirkan file excel")
-filename = tgl+".xlsx"
-attachment = open(filename, "rb")
-p = MIMEBase('application', 'octet-stream')
-p.set_payload((attachment).read())
-encoders.encode_base64(p)
-p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-msg.attach(p)
-print("mengirim email....ke "+ ", ".join(toaddr))
-s = smtplib.SMTP('smtp.gmail.com', 587)
-s.starttls()
-s.login(fromaddr, "eaauyyswikrxswzn")
-text = msg.as_string()
-s.sendmail(fromaddr, toaddr, text)
-print("email terkirim ke " + ", ".join(toaddr))
-print("mengakhiri sesi...")
-s.quit()
+
+def kirim_ems():
+    fromaddr = "thomihidayat1@gmail.com"
+    toaddr = ["hidayatthomi@gmail.com", "0001indahratna@gmail.com"]
+    tgl = datetime.today().strftime("%d-%m-%y")
+    # membuat MIMEMultipart
+    print("membuat pesan....")
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = ", ".join(toaddr)
+    msg['Subject'] = "EMS Daily Report " + tgl
+    body = "Please find the attached file here.."
+    msg.attach(MIMEText(body, 'plain'))
+    print("pesan jadi ...! melampirkan file excel")
+    filename = tgl + ".xlsx"
+    attachment = open(filename, "rb")
+    p = MIMEBase('application', 'octet-stream')
+    p.set_payload((attachment).read())
+    encoders.encode_base64(p)
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    msg.attach(p)
+    print("mengirim email....ke " + ", ".join(toaddr))
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(fromaddr, "eaauyyswikrxswzn")
+    text = msg.as_string()
+    s.sendmail(fromaddr, toaddr, text)
+    print("email terkirim ke " + ", ".join(toaddr))
+    print("laporan EMS tanggal..."+ tgl +" telah dikirim")
+    print("menunngu laporan hari esok trus kirim lagi..")
+    s.quit()
+
+
+schedule.every().day.at("23:36").do(kirim_ems)
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
