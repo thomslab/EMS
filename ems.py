d06bbc15043a427d95ec25a411e973a426e9da35
mitohida <hidayatthomi@gmail.com>
2022-07-06 14:04:57 +0800

    initial commit

--- ems.py
+++ ems.py
@@ -9,10 +9,14 @@ from email import encoders
 
 
 def kirim_ems():
+    # alamat email
     fromaddr = "thomihidayat1@gmail.com"
-    toaddr = ["hidayatthomi@gmail.com", "0001indahratna@gmail.com"]
+    toaddr = ["hidayatthomi@gmail.com", "zainalbin1009@gmail.com"]
+
+    #mengambil tanggal hari ini
     tgl = datetime.today().strftime("%d-%m-%y")
-    # membuat MIMEMultipart
+
+    # membuat instance MIMEMultipart
     print("membuat pesan....")
     msg = MIMEMultipart()
     msg['From'] = fromaddr
@@ -23,6 +27,7 @@ def kirim_ems():
     print("pesan jadi ...! melampirkan file excel")
 
     try:
+        #melampirkan file excel terbaru / hari ini
         filename = tgl + ".xlsx"
         attachment = open(filename, "rb")
         p = MIMEBase('application', 'octet-stream')
@@ -30,6 +35,8 @@ def kirim_ems():
         encoders.encode_base64(p)
         p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
         msg.attach(p)
+
+        #mengirim email
         print("mengirim email....ke " + ", ".join(toaddr))
         s = smtplib.SMTP('smtp.gmail.com', 587)
         s.starttls()
@@ -39,9 +46,12 @@ def kirim_ems():
         print("email terkirim ke " + ", ".join(toaddr))
         print("laporan EMS tanggal..." + tgl + " telah dikirim")
         print("menunngu laporan hari esok trus kirim lagi..")
+
+        #mengakhiri session
         s.quit()
 
     except FileNotFoundError:
+        #exception jika file tidak ditemukan
         print("file tidak ditemukan")
         body = "[FILE TIDAK DITEMUKAN]"
         msg.attach(MIMEText(body, 'plain'))
@@ -52,9 +62,17 @@ def kirim_ems():
         s.sendmail(fromaddr, toaddr, text)
         s.quit()
 
+# meng schedule pengiriman setiap hari pada jam
+schedule.every().day.at("14:03").do(kirim_ems)
 
-schedule.every().day.at("00:02").do(kirim_ems)
-schedule.every().day.at("00:03").do(kirim_ems)
+#cek jam tiap 1 detik
+i = 0
 while 1:
     schedule.run_pending()
+    #Print program jalan 1x
+    if i == 0:
+        print('Program Automail Jalan...')
+        i +=1
+    else:
+        pass
     time.sleep(1)
